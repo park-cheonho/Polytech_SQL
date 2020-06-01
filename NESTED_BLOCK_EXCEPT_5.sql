b1ef7aa4b3a5976335355073c2d92b7b95c6ba4a
park-cheonho <cheonho08@gmail.com>
2020-06-01 22:25:48 +0900

    [ADD]2020_06_01_PLSQL

---
+++ NESTED_BLOCK_EXCEPT_5.sql
@@ -0,0 +1,29 @@
+
+BEGIN 
+    INSERT INTO DEPT VALUES(66, 'OUTER_BLK_PART', 'Main_Blk');
+    
+    <<Nested_BLOCK_1>>
+    BEGIN
+            INSERT INTO DEPT VALUES(76, 'LOCAL_PART_1', 'NESTED_Blk1');        
+            INSERT INTO DEPT VALUES(777, 'LOCAL_PART_1', 'NESTED_Blk1'); -- 런타임 에러 발생
+            INSERT INTO DEPT VALUES(77, 'LOCAL_PART_1', 'NESTED_Blk1');
+            INSERT INTO DEPT VALUES(78, 'LOCAL_PART_1', 'NESTED_Blk1');
+            COMMIT;
+    EXCEPTION
+            WHEN NO_DATA_FOUND THEN
+                NULL;
+	WHEN OTHERS THEN
+	    NULL;
+    END Nested_BLOCK_1;
+    
+    <<Nested_BLOCK_2>>
+    BEGIN
+            INSERT INTO DEPT VALUES(88, 'LOCAL_PART_1', 'NESTED_Blk1');
+            COMMIT;
+    END Nested_BLOCK2;
+    
+    INSERT INTO DEPT VALUES(99, 'OUTER_BLK_PART', 'Main_Blk');
+END;
+/
+
+SELECT * FROM DEPT;
---
+++ PLSQL_3.sql
@@ -0,0 +1,8 @@
+@@C:\Users\HP\Desktop\myGit\SQL\3_1_Nested_blk.sql
+
+SELECT * FROM DEPT;
+
+@@C:\Users\HP\Desktop\myGit\SQL\3_NESTED_BLOCK_TRANS.sql
+
+@@C:\Users\HP\Desktop\myGit\SQL\3_NESTED_BLOCK_EXCEPT_1.sql
+@@C:\Users\HP\Desktop\myGit\SQL\3_NESTED_BLOCK_EXCEPT_2.sql
